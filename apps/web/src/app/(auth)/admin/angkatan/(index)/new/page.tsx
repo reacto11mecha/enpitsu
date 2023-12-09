6901024c0953e383fcdafe1968b785747fa1fe54
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-12-09 08:52:00 +0700

    feat: menambahkan skeleton

--- apps/web/src/app/(auth)/admin/angkatan/(index)/new/page.tsx
+++ apps/web/src/app/(auth)/admin/angkatan/(index)/new/page.tsx
@@ -1,53 +1,22 @@
-import * as React from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CardWithForm() {
   return (
-    <div className="mt-5 px-5">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>
-            Deploy your new project in one-click.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                {/* <Label htmlFor="name">Name</Label>
-                            <Input id="name" placeholder="Name of your project" /> */}
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                {/* <Label htmlFor="framework">Framework</Label>
-                            <Select>
-                                <SelectTrigger id="framework">
-                                    <SelectValue placeholder="Select" />
-                                </SelectTrigger>
-                                <SelectContent position="popper">
-                                    <SelectItem value="next">Next.js</SelectItem>
-                                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                                    <SelectItem value="astro">Astro</SelectItem>
-                                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                                    </SelectContent>
-                                </Select> */}
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
-        </CardFooter>
-      </Card>
+    <div className="mt-5 flex w-full flex-col justify-center gap-8 px-5">
+      <h2 className="scroll-m-20 text-center text-3xl font-semibold tracking-tight first:mt-0">
+        Buat Angkatan Baru
+      </h2>
+
+      <Suspense
+        fallback={
+          <div className="flex w-full flex-col items-center">
+            <Skeleton className="h-[25em] w-full md:w-[35em]" />
+          </div>
+        }
+      >
+        <p>TEST</p>
+      </Suspense>
     </div>
   );
 }
