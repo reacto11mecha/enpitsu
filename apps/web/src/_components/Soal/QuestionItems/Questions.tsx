9ccbdaa343404481fb6abf0f2c25919b25c5e53d
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-12-23 10:34:03 +0700

    feat: checkpoint

---
+++ apps/web/src/_components/Soal/QuestionItems/Questions.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusCircle } from "lucide-react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { EssayQuestion } from "./EssayQuestion";
+import { MultipleChoice } from "./MultipleChoice";
+
+const formSchema = z.object({
+  multipleChoice: z
+    .array(
+      z.object({
+        question: z.string().min(1, { message: "Pertanyaan wajib di isi!" }),
+        options: z
+          .array(
+            z.object({
+              order: z.number().min(1).max(5),
+              answer: z
+                .string()
+                .min(1, { message: "Opsi jawaban wajib di isi!" }),
+            }),
+          )
+          .min(5)
+          .max(5),
+
+        // correct answer by options order
+        correctAnswer: z.number(),
+      }),
+    )
+    .min(1),
+
+  essay: z
+    .array(
+      z.object({
+        question: z.string().min(1, { message: "Pertanyaan wajib di isi!" }),
+        answer: z.string().min(1, { message: "Jawaban harus di isi!" }),
+      }),
+    )
+    .min(1),
+});
+
+export const Questions = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      multipleChoice: [],
+      essay: [],
+    },
+  });
+
+  const mutlipleChoiceField = useFieldArray({
+    control: form.control,
+    name: "multipleChoice",
+  });
+
+  const essayField = useFieldArray({
+    control: form.control,
+    name: "essay",
+  });
+
+  return (
+    <div className="flex flex-col gap-8 pb-10">
+      <div className="flex flex-col gap-4">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Pilihan Ganda
+        </h3>
+
+        <div className="flex flex-col gap-5">
+          {mutlipleChoiceField.fields.map((field) => (
+            <MultipleChoice key={field.id} />
+          ))}
+
+          <Button
+            variant="outline"
+            className="h-full w-full p-5"
+            onClick={() =>
+              mutlipleChoiceField.append({
+                question: "",
+                options: Array.from({ length: 5 }).map((_, idx) => ({
+                  order: idx + 1,
+                  answer: "",
+                })),
+                correctAnswer: 0,
+              })
+            }
+          >
+            <PlusCircle className="h-6 w-6" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Esai
+        </h3>
+
+        <div className="flex flex-col gap-5">
+          {essayField.fields.map((field) => (
+            <EssayQuestion key={field.id} />
+          ))}
+
+          <Button
+            variant="outline"
+            className="h-full w-full p-5"
+            onClick={() =>
+              essayField.append({
+                question: "",
+                answer: "",
+              })
+            }
+          >
+            <PlusCircle className="h-6 w-6" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- apps/web/src/app/(auth)/admin/soal/butir/[id]/page.tsx
+++ apps/web/src/app/(auth)/admin/soal/butir/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { db, eq, schema } from "@enpitsu/db";
 
+import { Questions } from "~/_components/Soal/QuestionItems/Questions";
+
 export default async function QuestionItemsPage({
   params,
 }: {
@@ -18,5 +20,19 @@ export default async function QuestionItemsPage({
 
   console.log(question);
 
-  return <></>;
+  return (
+    <div className="mt-5 flex flex-col gap-7 px-5 py-5 md:items-center">
+      <div className="w-full md:w-[80%]">
+        <div className="mb-5 space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">Butir Soal</h2>
+          <p className="text-muted-foreground w-full md:w-[80%] lg:w-[70%]">
+            Tambah, ubah, edit, dan hapus soal pilihan ganda dan esai pada
+            halaman ini. Jangan lupa tentukan jawaban benar pada setiap soal.
+          </p>
+        </div>
+
+        <Questions />
+      </div>
+    </div>
+  );
 }
