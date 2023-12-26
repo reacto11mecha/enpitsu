c39fa66d53e384ecfbdc99cbc6e48cbf0c557901
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-12-27 03:46:15 +0700

    fix: lint (test turbo cache)

--- apps/exam-web/src/components/ui/button.tsx
+++ apps/exam-web/src/components/ui/button.tsx
@@ -34,7 +34,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
@@ -52,4 +52,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export { Button, buttonVariants };
