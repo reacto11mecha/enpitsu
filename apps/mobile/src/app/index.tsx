f17b95028ba4bdf55020633443bbb3327b3afe0c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-01-03 19:23:02 +0700

    fix: eslint

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { Button, Pressable, Text, TextInput, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Link, Stack } from "expo-router";
-import { FlashList } from "@shopify/flash-list";
+import { Button, SafeAreaView, Text, View } from "react-native";
 import { useColorScheme } from "nativewind";
-
-import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
 
 const Index = () => {
   const { colorScheme, toggleColorScheme } = useColorScheme();
