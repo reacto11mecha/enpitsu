f77d36ff32f46c7ca42b6d516a75458537b97512
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-01-19 16:27:15 +0700

    feat: checkpoint lagi

--- packages/api/src/router/admin.ts
+++ packages/api/src/router/admin.ts
@@ -1,8 +1,7 @@
-// import { cache } from "@enpitsu/cache";
+import { cache } from "@enpitsu/cache";
 import { and, eq, not, schema, sql } from "@enpitsu/db";
-
-// import { TRPCError } from "@trpc/server";
-// import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 import { adminProcedure, createTRPCRouter } from "../trpc";
 
@@ -15,4 +14,61 @@ export const adminRouter = createTRPCRouter({
       ),
     }),
   ),
+
+  rejectPendingUser: adminProcedure
+    .input(z.object({ id: z.string() }))
+    .mutate(({ ctx, input }) =>
+      ctx.db.transaction(async (tx) => {
+        const specificUser = await tx.query.users.findFirst({
+          where: eq(schema.users.id, input.id),
+        });
+
+        if (!specificUser)
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Pengguna yang dituju tidak ditemukan!",
+          });
+
+        if (specificUser.emailVerified)
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Pengguna sudah di approve!",
+          });
+
+        return tx.delete(schema.users).where(eq(schema.users.id, input.id));
+      }),
+    ),
+
+  acceptPendingUser: adminProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        role: z.string,
+        role: z.enum(["admin", "user"]),
+      }),
+    )
+    .mutate(({ ctx, input }) =>
+      ctx.db.transaction(async (tx) => {
+        const specificUser = await tx.query.users.findFirst({
+          where: eq(schema.users.id, input.id),
+        });
+
+        if (!specificUser)
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Pengguna yang dituju tidak ditemukan!",
+          });
+
+        if (specificUser.emailVerified)
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Pengguna sudah di approve!",
+          });
+
+        return await tx.update(schema.users).set({
+          emailVerified: new Date(),
+          role: input.role,
+        });
+      }),
+    ),
 });
