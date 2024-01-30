ce9b30d6527eb0db61462e728fcb0ade20ae28e4
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-01-30 10:45:00 +0700

    fix: polyfill js untuk support browser modern yang "legacy"

--- apps/exam-web/src/Provider.tsx
+++ apps/exam-web/src/Provider.tsx
@@ -1,17 +1,32 @@
-import SetToken from "@/components/set-token";
+import { lazy, Suspense } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { studentTokenAtom } from "@/lib/atom";
 import { useAtom } from "jotai";
+import { RefreshCw } from "lucide-react";
 
 import App from "./App";
+
+const SetToken = lazy(() => import("@/components/set-token"));
 
 export default function Provider() {
   const [studentAtom] = useAtom(studentTokenAtom);
 
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      {studentAtom === "" ? <SetToken init /> : <App />}
+      {studentAtom === "" ? (
+        <Suspense
+          fallback={
+            <div className="flex h-screen w-screen items-center justify-center">
+              <RefreshCw size={35} className="animate-spin" />
+            </div>
+          }
+        >
+          <SetToken init />
+        </Suspense>
+      ) : (
+        <App />
+      )}
       <Toaster />
     </ThemeProvider>
   );
--- apps/exam-web/vite.config.ts
+++ apps/exam-web/vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
+import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      renderLegacyChunks: true,
+      modernPolyfills: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
