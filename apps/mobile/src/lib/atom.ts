c8b8b80e8c2e3b935d605a3bc1b64ab632a081e7
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-02-03 15:09:53 +0700

    fix: memperbaiki value tidak tersimpan

--- apps/mobile/src/lib/atom.ts
+++ apps/mobile/src/lib/atom.ts
@@ -1,6 +1,37 @@
-import { atomWithStorage } from "jotai/utils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { add, formatISO } from "date-fns";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
-export const studentTokenAtom = atomWithStorage<string>("token", "");
+const atomWithAsyncStorage = <T>(key: string, initialValue: T) => {
+  const storage = createJSONStorage<T>(() => AsyncStorage);
+
+  // preserve original getItem and setItem
+  const { getItem, setItem } = storage;
+
+  // override setItem, expire in 2 weeks
+  storage.setItem = (_, value, expireInHours = 336) => {
+    // add expireAt to newValue
+    const expireAt = add(new Date(), { hours: expireInHours });
+    const updatedValue = { ...value, expireAt: formatISO(expireAt) };
+
+    // updatedValue is a JSON object -- createJSONStorage handles that for us
+    // call original setItem with updatedValue
+    return setItem(key, updatedValue);
+  };
+
+  // override getItem
+  storage.getItem = async () => {
+    // call original getItem
+    const value = await getItem(key, initialValue);
+
+    // value is already a JSON object -- createJSONStorage handles that for us
+    return value;
+  };
+
+  return atomWithStorage(key, initialValue, storage);
+};
+
+export const studentTokenAtom = atomWithAsyncStorage<string>("token", "");
 
 export interface TStudentAnswer {
   slug: string;
@@ -16,7 +47,7 @@ export interface TStudentAnswer {
   }[];
 }
 
-export const studentAnswerAtom = atomWithStorage<TStudentAnswer[]>(
+export const studentAnswerAtom = atomWithAsyncStorage<TStudentAnswer[]>(
   "studentAnswer",
   [],
 );
