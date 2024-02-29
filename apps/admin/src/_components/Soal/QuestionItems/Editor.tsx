ad6816200e75d8bba03d42d56ebaf38f11bdd349
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-02-29 14:37:39 +0700

    feat: menambahkan fitur audio untuk input soal

--- apps/admin/src/_components/Soal/QuestionItems/Editor.tsx
+++ apps/admin/src/_components/Soal/QuestionItems/Editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRef } from "react";
 import katex from "katex";
 // @ts-expect-error there's actually a type for this package, but dont know why this still yell for a missing type
 import Delta from "quill-delta";
@@ -8,6 +9,36 @@ import type { ReactQuillProps } from "react-quill";
 
 import "katex/dist/katex.min.css";
 import "react-quill/dist/quill.snow.css";
+
+const Quill = ReactQuill.Quill;
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const Embed = Quill.import("blots/block/embed");
+
+class AudioBlot extends Embed {
+  static blotName: string;
+  static tagName: string;
+
+  static create(url: string) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    const node = super.create() as HTMLAudioElement;
+
+    node.setAttribute("src", url);
+    node.setAttribute("controls", "");
+
+    node.style.width = "100%";
+
+    return node;
+  }
+
+  static value(node: HTMLAudioElement) {
+    return node.getAttribute("src");
+  }
+}
+AudioBlot.blotName = "audio";
+AudioBlot.tagName = "audio";
+
+Quill.register(AudioBlot);
 
 window.katex = katex;
 
@@ -61,12 +92,34 @@ export default function Editor({
   value: ReactQuill.Value | undefined;
   setValue: (val: string) => void;
 }) {
+  const quillRef = useRef<ReactQuill>(null!);
+
   return (
-    <ReactQuill
-      theme="snow"
-      value={value}
-      modules={quillModules}
-      onChange={setValue}
-    />
+    <div className="flex w-full flex-col gap-2">
+      <ReactQuill
+        ref={quillRef}
+        theme="snow"
+        value={value}
+        modules={quillModules}
+        onChange={setValue}
+      />
+
+      <button
+        onClick={() => {
+          if (quillRef.current) {
+            quillRef.current
+              .getEditor()
+              .insertEmbed(
+                quillRef.current.selection?.index ?? 0,
+                "audio",
+                "https://download.samplelib.com/mp3/sample-6s.mp3",
+                "user",
+              );
+          }
+        }}
+      >
+        ASD
+      </button>
+    </div>
   );
 }
