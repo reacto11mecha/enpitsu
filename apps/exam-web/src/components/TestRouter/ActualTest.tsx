2e1494c0439fd9f9ee6eae769bdda23c6de279e2
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-03-08 22:40:31 +0700

    feat: menambahkan fitur baru

--- apps/exam-web/src/components/TestRouter/ActualTest.tsx
+++ apps/exam-web/src/components/TestRouter/ActualTest.tsx
@@ -10,14 +10,16 @@ import {
 } from "@/components/ui/form";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
+import { useToast } from "@/components/ui/use-toast";
 import { useCountdown } from "@/hooks/useCountdown";
 import { useNetworkState } from "@/hooks/useNetworkState";
 import { usePageVisibility } from "@/hooks/usePageVisibility";
-import { studentAnswerAtom } from "@/lib/atom";
+import { studentAnswerAtom, studentTokenAtom } from "@/lib/atom";
 import { api } from "@/utils/api";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useSetAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import katex from "katex";
 import { ArrowLeft, Loader2 } from "lucide-react";
 import { useFieldArray, useForm } from "react-hook-form";
@@ -27,8 +29,8 @@ import { useDebounceCallback } from "usehooks-ts";
 import { z } from "zod";
 
 import { ModeToggle } from "../mode-toggle";
-import { useToast } from "../ui/use-toast";
 import {
+  AnsweredQuestionsList,
   BadInternetAlert,
   DishonestyAlert,
   DishonestyCountAlert,
@@ -60,7 +62,11 @@ export const CountdownIsolation = memo(function Countdown({
     if (isEnded) theEndHasCome();
   }, [isEnded, theEndHasCome]);
 
-  return <Button variant="outline">{countdown}</Button>;
+  return (
+    <Button variant="outline" className="font-space">
+      {countdown}
+    </Button>
+  );
 });
 
 const Test = ({ data, initialData }: Props) => {
@@ -76,6 +82,7 @@ const Test = ({ data, initialData }: Props) => {
 
   const { toast } = useToast();
 
+  const studentToken = useAtomValue(studentTokenAtom);
   const setStudentAnswers = useSetAtom(studentAnswerAtom);
 
   const blocklistMutation = api.exam.storeBlocklist.useMutation({
@@ -120,6 +127,7 @@ const Test = ({ data, initialData }: Props) => {
   const [canUpdateDishonesty, setCanUpdateDishonesty] = useState(true);
 
   const [dishonestyWarning, setDishonestyWarning] = useState(false);
+  const [answeredDrawerOpen, setDrawerOpen] = useState(false);
   const [badInternetAlert, setBadInternet] = useState(false);
   const [wakeLockError, setWakeLockError] = useState(false);
 
@@ -437,7 +445,7 @@ const Test = ({ data, initialData }: Props) => {
 
       <header className="no-copy fixed inset-x-0 top-0 z-50 flex w-full justify-center border-solid">
         <div className="flex h-full w-full flex-wrap items-center justify-center gap-2 border border-b bg-white p-2 px-5 dark:bg-stone-900 sm:gap-4">
-          <GoToHome />
+          <GoToHome canUpdateDishonesty={setCanUpdateDishonesty} />
 
           <DishonestyCountAlert dishonestyCount={dishonestyCount} />
 
@@ -446,93 +454,153 @@ const Test = ({ data, initialData }: Props) => {
             theEndHasCome={theEndHasCome}
           />
 
+          <AnsweredQuestionsList
+            open={answeredDrawerOpen}
+            toggleDrawer={setDrawerOpen}
+            slug={data.slug}
+            multipleChoices={multipleChoicesField.fields}
+            essays={essaysField.fields}
+          />
+
           <ModeToggle size="default" />
         </div>
       </header>
 
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(onSubmit)}
+          onSubmit={form.handleSubmit(onSubmit, () => setDrawerOpen(true))}
           className="no-copy flex justify-center px-4 pb-16 pt-20"
         >
           <div className="flex w-full max-w-lg flex-col gap-8">
-            <div className="flex flex-col gap-4">
-              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                Pilihan Ganda
-              </h3>
+            <Card>
+              <div className="h-3 rounded-t-lg rounded-tr-lg bg-green-700 dark:bg-green-800" />
+              <CardHeader>
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  {data.title}
+                </h4>
+              </CardHeader>
+              <Separator />
+              <CardContent className="p-6">
+                <ul>
+                  <li>
+                    Kode Soal : <span className="font-space">{data.slug}</span>
+                  </li>
+                  <li>
+                    Token Peserta :{" "}
+                    <span className="font-space">{studentToken}</span>
+                  </li>
+                </ul>
+              </CardContent>
+            </Card>
 
-              <div className="flex flex-col gap-5">
-                {multipleChoicesField.fields.map((field, index) => (
-                  <Card key={field.id} className="w-full">
-                    <CardHeader>
-                      <h3
-                        className="no-copy actual-question scroll-m-20 text-lg tracking-tight"
-                        dangerouslySetInnerHTML={{ __html: field.question }}
-                      />
-                    </CardHeader>
-                    <CardContent>
-                      <FormField
-                        control={form.control}
-                        name={`multipleChoices.${index}.choosedAnswer` as const}
-                        render={({ field: currentField }) => (
-                          <FormItem className="space-y-3">
-                            <FormControl>
-                              <RadioGroup
-                                className="space-y-2"
-                                value={String(currentField.value)}
-                                onValueChange={(val) => {
-                                  currentField.onChange(parseInt(val));
+            {multipleChoicesField.fields.length >= 1 ? (
+              <div className="flex flex-col gap-4">
+                <Card>
+                  <CardHeader className="rounded-t-lg rounded-tr-lg bg-green-700 dark:bg-green-800">
+                    <h4 className="scroll-m-20 text-xl font-semibold uppercase tracking-tight text-green-50">
+                      Soal Pilihan Ganda
+                    </h4>
+                  </CardHeader>
+                  <Separator />
+                  <CardContent className="p-6">
+                    <p className="font-semibold leading-7 [&:not(:first-child)]:mt-6">
+                      Pilih salah satu jawaban yang paling benar di antara lima
+                      (5) opsi yang ada!
+                    </p>
+                  </CardContent>
+                </Card>
 
-                                  multipleChoiceDebounced({
-                                    iqid: field.iqid,
-                                    choosedAnswer: parseInt(val),
-                                  });
-                                }}
-                                disabled={submitAnswerMutation.isLoading}
-                              >
-                                {field.options.map((option, idx) => (
-                                  <div
-                                    className="flex items-center space-x-2"
-                                    key={`options.${field.iqid}.opt.${idx}`}
-                                  >
-                                    <RadioGroupItem
-                                      value={String(option.order)}
-                                      id={`options.${field.iqid}.opt.${idx}`}
-                                      disabled={submitAnswerMutation.isLoading}
-                                    />
-                                    <Label
-                                      htmlFor={`options.${field.iqid}.opt.${idx}`}
-                                      className="no-copy text-base font-normal"
-                                      dangerouslySetInnerHTML={{
-                                        __html: option.answer,
-                                      }}
-                                    />
-                                  </div>
-                                ))}
-                              </RadioGroup>
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </CardContent>
-                  </Card>
-                ))}
+                <div className="flex flex-col gap-5">
+                  {multipleChoicesField.fields.map((field, index) => (
+                    <Card
+                      key={field.id}
+                      className="w-full"
+                      id={`choice-${field.iqid}`}
+                    >
+                      <CardHeader>
+                        <h3
+                          className="no-copy actual-question scroll-m-20 text-lg tracking-tight"
+                          dangerouslySetInnerHTML={{ __html: field.question }}
+                        />
+                      </CardHeader>
+                      <CardContent>
+                        <FormField
+                          control={form.control}
+                          name={
+                            `multipleChoices.${index}.choosedAnswer` as const
+                          }
+                          render={({ field: currentField }) => (
+                            <FormItem className="space-y-3">
+                              <FormControl>
+                                <RadioGroup
+                                  className="space-y-2"
+                                  value={String(currentField.value)}
+                                  onValueChange={(val) => {
+                                    currentField.onChange(parseInt(val));
+
+                                    multipleChoiceDebounced({
+                                      iqid: field.iqid,
+                                      choosedAnswer: parseInt(val),
+                                    });
+                                  }}
+                                  disabled={submitAnswerMutation.isLoading}
+                                >
+                                  {field.options.map((option, idx) => (
+                                    <div
+                                      className="flex items-center space-x-2"
+                                      key={`options.${field.iqid}.opt.${idx}`}
+                                    >
+                                      <RadioGroupItem
+                                        value={String(option.order)}
+                                        id={`options.${field.iqid}.opt.${idx}`}
+                                        disabled={
+                                          submitAnswerMutation.isLoading
+                                        }
+                                      />
+                                      <Label
+                                        htmlFor={`options.${field.iqid}.opt.${idx}`}
+                                        className="no-copy w-full text-base font-normal"
+                                        dangerouslySetInnerHTML={{
+                                          __html: option.answer,
+                                        }}
+                                      />
+                                    </div>
+                                  ))}
+                                </RadioGroup>
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </CardContent>
+                    </Card>
+                  ))}
+                </div>
               </div>
-            </div>
+            ) : null}
 
             {essaysField.fields.length >= 1 ? (
               <div className="flex flex-col gap-3">
-                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                  Esai
-                </h3>
+                <Card>
+                  <CardHeader className="rounded-t-lg rounded-tr-lg bg-green-700 dark:bg-green-800">
+                    <h4 className="scroll-m-20 text-xl font-semibold uppercase tracking-tight text-green-50">
+                      Soal Esai
+                    </h4>
+                  </CardHeader>
+                  <Separator />
+                  <CardContent className="p-6">
+                    <p className="font-semibold leading-7 [&:not(:first-child)]:mt-6">
+                      Isi jawaban esai dengan jawaban paling benar!
+                    </p>
+                  </CardContent>
+                </Card>
 
                 <div className="flex flex-col gap-5">
                   {essaysField.fields.map((field, index) => (
-                    <Card key={field.iqid}>
+                    <Card key={field.iqid} id={`essay-${field.iqid}`}>
                       <CardHeader>
                         <h3
-                          className="no-copy scroll-m-20 text-base tracking-tight"
+                          className="no-copy actual-question scroll-m-20 text-base tracking-tight"
                           dangerouslySetInnerHTML={{ __html: field.question }}
                         />
                       </CardHeader>
