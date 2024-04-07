3c7a6d96bdeea201c88ee0a748e7cd8c4afada8d
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-04-07 17:09:50 +0700

    fix: nativewind

--- apps/mobile/src/app/(root)/index.tsx
+++ apps/mobile/src/app/(root)/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import { Link, Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context"
+// import { Link, Stack } from "expo-router";
 
 export default function HomePage() {
   return <SafeAreaView className="bg-background"></SafeAreaView>;
