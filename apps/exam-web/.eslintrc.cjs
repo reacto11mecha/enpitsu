a6ec147826791eb23e13d72a54b9447cb1deac77
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-06-08 12:22:25 +0700

    feat: penambahan custom config (baru persiapan)

--- apps/exam-web/.eslintrc.cjs
+++ apps/exam-web/.eslintrc.cjs
@@ -1,3 +1,40 @@
+const customAntiCheatCheckerOn = {
+  create(context) {
+    return {
+      VariableDeclaration(node) {
+        node.declarations.forEach(declaration => {
+          if (
+            declaration.init &&
+            declaration.init.type === 'CallExpression' &&
+            declaration.init.callee.name === 'useState' &&
+            declaration.init.arguments.length > 0 &&
+            declaration.init.arguments[0].type === 'Literal' &&
+            declaration.init.arguments[0].value === true &&
+            declaration.id.type === 'ArrayPattern' &&
+            declaration.id.elements.length === 2 &&
+            declaration.id.elements[0].name === 'canUpdateDishonesty' &&
+            declaration.id.elements[1].name === 'setCanUpdateDishonesty'
+          ) {
+            context.report({
+              node: declaration.init.arguments[0],
+              message: 'State variable "canUpdateDishonesty" initial value must be true.',
+              fix(fixer) {
+                return fixer.replaceText(declaration.init.arguments[0], 'true');
+              }
+            });
+          }
+        });
+      }
+    };
+  }
+};
+
+const eptsPlugin = {
+  rules: {
+    "enforce-anti-check-initial-true": customAntiCheatCheckerOn
+  }
+}
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -8,7 +45,10 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ['react-refresh'],
+  // plugins: {
+  //   "enpitsu-custom-lint": eptsPlugin
+  // },
   rules: {
     "react-refresh/only-export-components": [
       "warn",
