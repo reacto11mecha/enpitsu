153c94faefd3805eec22e9c2b7aa39980bd61fba
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2025-02-01 20:18:09 +0700

    feat: menghapus native

--- apps/mobile/modules/detect-screen-sleeping/android/src/main/java/expo/modules/detectscreensleeping/DetectScreenSleepingView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.detectscreensleeping
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class DetectScreenSleepingView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
--- apps/mobile/modules/detect-screen-sleeping/src/DetectScreenSleeping.types.ts
+++
@@ -1,7 +0,0 @@
-export type ChangeEventPayload = {
-  value: string;
-};
-
-export type DetectScreenSleepingViewProps = {
-  name: string;
-};
