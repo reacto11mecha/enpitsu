f7c948055a580251be1928ad00fdd43c79ab3508
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2025-02-05 22:58:23 +0700

    fix: memperbaiki susunan tabel

--- packages/db/src/schema/auth.ts
+++ packages/db/src/schema/auth.ts
@@ -1,14 +1,5 @@
-import type { AdapterAccount } from "@auth/core/adapters";
 import { relations } from "drizzle-orm";
-import {
-  boolean,
-  integer,
-  pgEnum,
-  pgTable,
-  primaryKey,
-  text,
-  timestamp,
-} from "drizzle-orm/pg-core";
+import { pgEnum, primaryKey } from "drizzle-orm/pg-core";
 
 // import { drizzle } from "drizzle-orm/postgres-js";
 // import postgres from "postgres";
@@ -18,151 +9,61 @@ import { questions } from "./question";
 
 export const roleEnum = pgEnum("role", ["admin", "user"]);
 
-export const users = myPgTable("user", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: text("name"),
-  email: text("email").unique(),
-  emailVerified: timestamp("emailVerified", { mode: "date" }),
+export const users = myPgTable("user", (t) => ({
+  id: t.uuid().notNull().primaryKey().defaultRandom(),
+  name: t.varchar({ length: 255 }),
+  email: t.varchar({ length: 255 }).notNull(),
+  emailVerified: t.timestamp({ mode: "date", withTimezone: true }),
   role: roleEnum("role").default("user").notNull(),
-  image: text("image"),
-});
+  image: t.varchar({ length: 255 }),
+}));
 
-export const usersRelations = relations(users, ({ many }) => ({
+export const userRelations = relations(users, ({ many }) => ({
+  accounts: many(accounts),
   questions: many(questions),
 }));
 
 export const accounts = myPgTable(
   "account",
-  {
-    userId: text("userId")
+  (t) => ({
+    userId: t
+      .uuid()
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
-    type: text("type").$type<AdapterAccount>().notNull(),
-    provider: text("provider").notNull(),
-    providerAccountId: text("providerAccountId").notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: text("token_type"),
-    scope: text("scope"),
-    id_token: text("id_token"),
-    session_state: text("session_state"),
-  },
-  (account) => [
-    {
-      compoundKey: primaryKey({
-        columns: [account.provider, account.providerAccountId],
-      }),
-    },
-  ],
+    type: t
+      .varchar({ length: 255 })
+      .$type<"email" | "oauth" | "oidc" | "webauthn">()
+      .notNull(),
+    provider: t.varchar({ length: 255 }).notNull(),
+    providerAccountId: t.varchar({ length: 255 }).notNull(),
+    refresh_token: t.varchar({ length: 255 }),
+    access_token: t.text(),
+    expires_at: t.integer(),
+    token_type: t.varchar({ length: 255 }),
+    scope: t.varchar({ length: 255 }),
+    id_token: t.text(),
+    session_state: t.varchar({ length: 255 }),
+  }),
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  }),
 );
 
-export const sessions = pgTable("session", {
-  sessionToken: text("sessionToken").primaryKey(),
-  userId: text("userId")
+export const accountRelations = relations(accounts, ({ one }) => ({
+  user: one(users, { fields: [accounts.userId], references: [users.id] }),
+}));
+
+export const sessions = myPgTable("session", (t) => ({
+  sessionToken: t.varchar({ length: 255 }).notNull().primaryKey(),
+  userId: t
+    .uuid()
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
-  expires: timestamp("expires", { mode: "date" }).notNull(),
-});
+  expires: t.timestamp({ mode: "date", withTimezone: true }).notNull(),
+}));
 
-export const verificationTokens = pgTable(
-  "verificationToken",
-  {
-    identifier: text("identifier").notNull(),
-    token: text("token").notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (verificationToken) => [
-    {
-      compositePk: primaryKey({
-        columns: [verificationToken.identifier, verificationToken.token],
-      }),
-    },
-  ],
-);
-
-export const authenticators = pgTable(
-  "authenticator",
-  {
-    credentialID: text("credentialID").notNull().unique(),
-    userId: text("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    providerAccountId: text("providerAccountId").notNull(),
-    credentialPublicKey: text("credentialPublicKey").notNull(),
-    counter: integer("counter").notNull(),
-    credentialDeviceType: text("credentialDeviceType").notNull(),
-    credentialBackedUp: boolean("credentialBackedUp").notNull(),
-    transports: text("transports"),
-  },
-  (authenticator) => [
-    {
-      compositePK: primaryKey({
-        columns: [authenticator.userId, authenticator.credentialID],
-      }),
-    },
-  ],
-);
-
-// export const users = myPgTable("user", {
-//   id: varchar("id", { length: 255 }).notNull().primaryKey(),
-//   name: varchar("name", { length: 255 }),
-//   email: varchar("email", { length: 255 }).notNull(),
-//   emailVerified: timestamp("emailVerified", {
-//     mode: "date",
-//   }),
-//   role: roleEnum("role").default("user").notNull(),
-//   image: varchar("image", { length: 255 }),
-// });
-
-// export const usersRelations = relations(users, ({ many }) => ({
-//   accounts: many(accounts),
-//   questions: many(questions),
-// }));
-
-// export const accounts = myPgTable(
-//   "account",
-//   {
-//     userId: varchar("userId", { length: 255 }).notNull(),
-//     type: varchar("type", { length: 255 })
-//       .$type<AdapterAccount["type"]>()
-//       .notNull(),
-//     provider: varchar("provider", { length: 255 }).notNull(),
-//     providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-//     refresh_token: varchar("refresh_token", { length: 255 }),
-//     access_token: varchar("access_token", { length: 255 }),
-//     expires_at: integer("expires_at"),
-//     token_type: varchar("token_type", { length: 255 }),
-//     scope: varchar("scope", { length: 255 }),
-//     id_token: text("id_token"),
-//     session_state: varchar("session_state", { length: 255 }),
-//   },
-//   (account) => ({
-//     compoundKey: primaryKey(account.provider, account.providerAccountId),
-//     userIdIdx: index("acc_userId_idx").on(account.userId),
-//   }),
-// );
-
-// export const accountsRelations = relations(accounts, ({ one }) => ({
-//   user: one(users, { fields: [accounts.userId], references: [users.id] }),
-// }));
-
-// export const sessions = myPgTable(
-//   "session",
-//   {
-//     sessionToken: varchar("sessionToken", { length: 255 })
-//       .notNull()
-//       .primaryKey(),
-//     userId: varchar("userId", { length: 255 }).notNull(),
-//     expires: timestamp("expires", { mode: "date" }).notNull(),
-//   },
-//   (session) => ({
-//     userIdIdx: index("sess_userId_idx").on(session.userId),
-//   }),
-// );
-
-// export const sessionsRelations = relations(sessions, ({ one }) => ({
-//   user: one(users, { fields: [sessions.userId], references: [users.id] }),
-// }));
+export const sessionRelations = relations(sessions, ({ one }) => ({
+  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
