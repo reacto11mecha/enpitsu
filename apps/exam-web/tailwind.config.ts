73929220fb6da9199b58e8ad8ae5c3f046b0b4b4
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2025-02-08 12:15:17 +0700

    fix: memperbaiki font yang salah pada exam-web

--- apps/exam-web/tailwind.config.ts
+++ apps/exam-web/tailwind.config.ts
@@ -5,11 +5,4 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: [...baseConfig.content, "../../packages/ui/src/*.{ts,tsx}"],
   presets: [baseConfig],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-manrope)", ...fontFamily.sans],
-      },
-    },
-  },
 } satisfies Config;
