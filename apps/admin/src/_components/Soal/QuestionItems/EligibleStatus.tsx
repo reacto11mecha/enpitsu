1cc2fdd1ded2d85a65984fb4c5bb38508c4d7c2a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2025-02-09 18:27:24 +0700

    feat: menambahkan memoization untuk status pengecekan

--- apps/admin/src/_components/Soal/QuestionItems/EligibleStatus.tsx
+++ apps/admin/src/_components/Soal/QuestionItems/EligibleStatus.tsx
@@ -1,5 +1,5 @@
 import type { RouterOutputs } from "@enpitsu/api";
-import { memo } from "react";
+import { memo, useMemo } from "react";
 import { cn } from "@enpitsu/ui";
 import { Button } from "@enpitsu/ui/button";
 import {
@@ -20,7 +20,7 @@ import {
 interface Props {
   isPending: boolean;
   isError: boolean;
-  data: RouterOutputs["question"]["getEligibleStatusFromQuestion"];
+  data: RouterOutputs["question"]["getEligibleStatusFromQuestion"] | undefined;
 }
 
 export const EligibleStatus = memo(function EligibleStatus({
@@ -28,31 +28,49 @@ export const EligibleStatus = memo(function EligibleStatus({
   isError,
   data,
 }: Props) {
+  const currentStatus = useMemo(() => {
+    if (isPending)
+      return { readable: "Sedang mengambil data status ke server..." };
+    if (isError) return { readable: "Gagal mengambil data status ke server." };
+
+    if (!data) return { readable: "Status data kosong, mohon menunggu..." };
+
+    switch (data.eligible) {
+      case "ELIGIBLE":
+        return { readable: "Status aman, peserta dapat mengerjakan soal ini" };
+      case "PROCESSING":
+        return { readable: "Sedang dilakukan pengecekan..." };
+      case "NOT_ELIGIBLE":
+      default:
+        return { readable: "Soal tidak layak dikerjakan" };
+    }
+  }, [isPending, isError, data]);
+
   return (
     <div className="w-full md:fixed md:bottom-2 md:right-2 md:w-fit">
       <Sheet>
         <SheetTrigger asChild>
           <Button
-            variant={isError ? "destructive" : ""}
+            variant={isError ? "destructive" : "default"}
             className="w-full md:w-fit"
           >
             {isPending ? (
               <Loader2 className="h-24 w-24 animate-spin" />
-            ) : data.eligible === "NOT_ELIGIBLE" || isError ? (
+            ) : data?.eligible === "NOT_ELIGIBLE" || isError ? (
               <OctagonX
                 className={cn("h-24 w-24", !isError && "text-red-500")}
               />
-            ) : data.eligible === "PROCESSING" ? (
+            ) : data?.eligible === "PROCESSING" ? (
               <LoaderPinwheel className="h-24 w-24 animate-spin" />
             ) : (
-              <LaptopMinimalCheck className="h-24 w-24" />
+              <LaptopMinimalCheck className="h-24 w-24 text-green-500" />
             )}
           </Button>
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
             <SheetTitle>Status Kelayakan Pengerjaan Soal</SheetTitle>
-            <SheetDescription>
+            <SheetDescription className="text-justify">
               Soal dapat dikatakan layak untuk dikerjakan oleh peserta apabila
               terdapat soal pilihan ganda atau soal esai atau ada keduanya.
               Setiap soal wajib memiliki pertanyaan dan opsi jawaban. Pada
@@ -60,9 +78,9 @@ export const EligibleStatus = memo(function EligibleStatus({
               perbaiki.
             </SheetDescription>
 
-            <p className="mt-2">Status: LOADING</p>
-
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <p className="mt-2 text-wrap text-sm">
+              Status saat ini: {currentStatus.readable}
+            </p>
           </SheetHeader>
         </SheetContent>
       </Sheet>
