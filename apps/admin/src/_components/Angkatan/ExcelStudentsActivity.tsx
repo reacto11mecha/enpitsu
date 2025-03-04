7d194da95d1a49465441bae0fb0dd1bf8db39cb8
Ezra Khairan Permana <48118327+reacto11mecha@users.noreply.github.com>
2025-03-04 19:53:13 +0700

    Update ExcelStudentsActivity.tsx

--- apps/admin/src/_components/Angkatan/ExcelStudentsActivity.tsx
+++ apps/admin/src/_components/Angkatan/ExcelStudentsActivity.tsx
@@ -51,8 +51,8 @@ const FileValueSchema = z.array(
           .max(50, { message: "Panjang maksimal hanya 50 karakter!" }),
         Token: z
           .string()
-          .min(6, { message: "Panjang token wajib 6 karakter!" })
-          .max(6, { message: "Panjang token tidak boleh dari 6 karakter!" })
+          .min(13, { message: "Panjang token wajib 6 karakter!" })
+          .max(13, { message: "Panjang token tidak boleh dari 6 karakter!" })
           .refine(validateId, { message: "Format token tidak sesuai!" }),
       }),
     ),
