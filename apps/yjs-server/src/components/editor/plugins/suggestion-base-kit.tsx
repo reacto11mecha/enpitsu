9d993724284c2aa37c3f2b009594531ca8bfb5f1
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2025-09-03 22:18:43 +0700

    fix: harusnya fix typingan yang terakhir

--- apps/yjs-server/src/components/editor/plugins/suggestion-base-kit.tsx
+++
@@ -1,6 +0,0 @@
-import { SuggestionLeafStatic } from "@/components/ui/suggestion-node-static";
-import { BaseSuggestionPlugin } from "@platejs/suggestion";
-
-export const BaseSuggestionKit = [
-  BaseSuggestionPlugin.withComponent(SuggestionLeafStatic),
-];
